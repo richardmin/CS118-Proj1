e8eb3bfb236dfa1f960909193cf7a321d7276209
Richard Min <richardmin97@gmail.com>
2016-05-05 22:52:21 -0700

    whoops bug

--- web-client.cpp
+++ web-client.cpp
@@ -290,7 +290,7 @@ int main(int argc, char* argv[])
 
     ss.str("");
     ssize_t x;
-    for(int i = 0; i < content_length; i+=x) //if content-length is defined, this will only get characters to the content-length. Otherwise, it functions as a while loop until no more bytes can be read.
+    for(int i = 0; i != content_length; i+=x) //if content-length is defined, this will only get characters to the content-length. Otherwise, it functions as a while loop until no more bytes can be read.
     {
       memset(buf, '\0', sizeof(buf));
       if ((x = recv(sockfd, buf, 20, 0)) == -1) {
